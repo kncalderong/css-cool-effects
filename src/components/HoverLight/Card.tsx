24741b3f87740ede846733191b1c83b1c5106792
Kevin Nicolas <kncalderong@gmail.com>
2023-05-27 16:00:31 -0500

    feat: hover effect finished with onMouseMove listener

--- src/components/HoverLight/Card.tsx
+++ src/components/HoverLight/Card.tsx
@@ -1,6 +1,7 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { useEffect, useRef, useState } from 'react'
 
 interface CardProps {
   icon: IconProp
@@ -9,15 +10,28 @@ interface CardProps {
 }
 
 const Card = ({ icon, title, content }: CardProps) => {
-
+  
+  const [isHover, setIsHover] = useState<boolean>(false)
+  const [coordinatesLightEffect, setCoordinatesLightEffect] = useState<[string,string]>(["",""])
+  const cardRef = useRef<HTMLInputElement>(null)
+   
+  
   const styleBefore = {
-    background: 'radial-gradient(800px circle at 132px 131px, rgba(255, 255, 255, 0.06), transparent 40%)'
+    background: `radial-gradient(800px circle at ${coordinatesLightEffect[0]} ${coordinatesLightEffect[1]}, rgba(255, 255, 255, 0.06), transparent 40%)`
+  }
+  
+  const styleAfter = {
+    background: `radial-gradient(600px circle at ${coordinatesLightEffect[0]} ${coordinatesLightEffect[1]}, rgba(255, 255, 255, 0.4), transparent 40%)`
+  }
+  
+  const hoverHandle = () => {
+    setCoordinatesLightEffect([cardRef.current!.style.cssText.split(";")[0].split(':')[1], cardRef.current!.style.cssText.split(";")[1].split(':')[1] ])
   }
 
   return (
-    <div className="card bg-[#ffffff1a] rounded-[10px] cursor-pointer flex flex-col relative w-[300px] h-[260px]">
-      <div id="likeBefore" className='rounded-[10px] h-full w-full left-0 top-0 absolute opacity-0 transition-opacity z-[3]' style={styleBefore}></div>
-      <div id='likeAfter' className='rounded-[10px] h-full w-full left-0 top-0 absolute opacity-0 transition-opacity'></div>
+    <div className="card bg-[#ffffff1a] rounded-[10px] cursor-pointer flex flex-col relative w-[300px] h-[260px]" ref={cardRef} onMouseOver={() => {setIsHover(true)}} onMouseLeave={() => { setIsHover(false) }} onMouseMove={hoverHandle}>
+      <div id="likeBefore" className={`rounded-[10px] h-full w-full left-0 top-0 absolute ${isHover ? 'opacity-100' : 'opacity-0'} transition-opacity z-[3]`} style={styleBefore}> </div>
+      <div id='likeAfter' className={`rounded-[10px] h-full w-full left-0 top-0 absolute transition-opacity ${isHover ? 'opacity-100' : 'opacity-0'} z-[1]`} style={styleAfter}></div>
       <div className="card-content bg-dark-gray rounded-[10px] flex flex-col flex-grow inset-[1px] p-[10px] absolute z-[2]">
         <div className="card-image flex justify-center items-center h-[140px] overflow-hidden">
           <FontAwesomeIcon icon={icon} className='text-[6em] opacity-25' color='#f0f0f0' />
--- src/components/HoverLight/HoverLight.tsx
+++ src/components/HoverLight/HoverLight.tsx
@@ -13,7 +13,7 @@ const HoverLight = () => {
           const rect = card.getBoundingClientRect(),
             x = e.clientX - rect.left,
             y = e.clientY - rect.top;
-
+          
           card.style.setProperty("--mouse-x", `${x}px`);
           card.style.setProperty("--mouse-y", `${y}px`);
         }
