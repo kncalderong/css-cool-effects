feccddbd8129db0b9dc10419fda3d474f8ceb204
Kevin Nicolas <kncalderong@gmail.com>
2023-05-27 16:58:46 -0500

    feat: functionallity finished taking advantage of css variables

--- src/components/HoverLight/HoverLight.tsx
+++ src/components/HoverLight/HoverLight.tsx
@@ -1,9 +1,11 @@
 import { faBuilding, faHorse, faBlenderPhone, faPersonThroughWindow, faPersonRunning, faOtter } from '@fortawesome/free-solid-svg-icons'
 import Card from './Card'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 
 const HoverLight = () => {
+  
+  const [hoverCards, sethoverCards] = useState<boolean>(false)
 
   useEffect(() => {
     const cards = document.getElementById("cards")
@@ -24,13 +26,13 @@ const HoverLight = () => {
 
   return (
     <section className='w-100 bg-dark-gray h-screen flex justify-center items-center' >
-      <div id="cards" className='flex flex-wrap gap-2 max-w-[916px] w-[calc(100%-20px)]' >
-        <Card icon={faBuilding} title='Apartments' content='Places to be apart. Wait, what?'  />
-        <Card icon={faHorse} title='Unicorns' content='A single corn. Er, I mean horn.'  />
-        <Card icon={faBlenderPhone} title='Blender Phones' content='These absolutely deserve to exist.'  />
-        <Card icon={faPersonThroughWindow} title='Adios' content='See you...'  />
-        <Card icon={faPersonRunning} title='I mean hello' content='...over here.'  />
-        <Card icon={faOtter} title='Otters' content='Look at me, imma cute lil fella.'/>
+      <div id="cards" className='flex flex-wrap gap-2 max-w-[916px] w-[calc(100%-20px)]' onMouseOver={() => { sethoverCards(true) }} onMouseLeave={() => { sethoverCards(false) }}>
+        <Card icon={faBuilding} title='Apartments' content='Places to be apart. Wait, what?' hoverCards={hoverCards}  />
+        <Card icon={faHorse} title='Unicorns' content='A single corn. Er, I mean horn.' hoverCards={hoverCards}  />
+        <Card icon={faBlenderPhone} title='Blender Phones' content='These absolutely deserve to exist.' hoverCards={hoverCards} />
+        <Card icon={faPersonThroughWindow} title='Adios' content='See you...' hoverCards={hoverCards}  />
+        <Card icon={faPersonRunning} title='I mean hello' content='...over here.' hoverCards={hoverCards} />
+        <Card icon={faOtter} title='Otters' content='Look at me, imma cute lil fella.' hoverCards={hoverCards} />
       </div>
     </section>
   )
