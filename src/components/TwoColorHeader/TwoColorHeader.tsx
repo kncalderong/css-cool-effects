cffcb9403ed7737b4ce1182f1f6eaccae2a57ec8
Kevin Nicolas <kncalderong@gmail.com>
2023-06-13 18:41:47 -0500

    feat: twoColorHeader drag is possible in touch devices as well

--- src/components/TwoColorHeader/TwoColorHeader.tsx
+++ src/components/TwoColorHeader/TwoColorHeader.tsx
@@ -3,14 +3,20 @@ import { useRef } from "react"
 
 const TwoColorHeader = () => {
   
-const leftSideRef = useRef<HTMLDivElement>(null)
+  const leftSideRef = useRef<HTMLDivElement>(null)
   
-  const handleMove = (e: React.MouseEvent) => {
-    leftSideRef.current!.style.width = `${e.clientX / window.innerWidth * 100}%`
+  const handleMove = (e: React.MouseEvent | React.TouchEvent) => {
+    if (e.nativeEvent instanceof MouseEvent) {      
+      leftSideRef.current!.style.width = `${e.nativeEvent.clientX / window.innerWidth * 100}%`
+    }
+    if (e.nativeEvent instanceof TouchEvent) {      
+      const touchPos = e.nativeEvent.touches[0]
+      leftSideRef.current!.style.width = `${ touchPos.clientX / window.innerWidth * 100}%`
+    }
   }
   
   return (
-    <section className='m-0 bg-dark-gray' onMouseMove={handleMove}>
+    <section className='m-0 bg-dark-gray' onMouseMove={handleMove} onTouchMove={handleMove}>
       <div id="left-side" className="side w-3/5 z-[2] h-screen place-items-center grid  overflow-hidden absolute bg-dark-gray" ref={leftSideRef} >
         <h2 className='title font-monserrat text-[7vw] font-extrabold mt-0 mx-[10vw] w-[80vw] text-white'>
           Today is going to be <span className="fancy font-lobster text-[8vw] text-[#f84f39]"> aweasome </span>
