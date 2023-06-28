78c87e39bd347bc06a13f7f3869aef3c50ad3d1b
Kevin Nicolas <kncalderong@gmail.com>
2023-06-28 13:33:57 -0500

    feat: slidingImageComponent finished including mobile devices

--- src/components/SlidingImageTrack/SlidingImageTrack.tsx
+++ src/components/SlidingImageTrack/SlidingImageTrack.tsx
@@ -13,7 +13,7 @@ const sliderImages = [
 
 const SlidingImageTrack = () => {
 
-  const [mouseDownAt, setMouseDownAt] = useState(0); //usf snippet
+  const [mouseDownAt, setMouseDownAt] = useState<number | null>(null); //usf snippet
   const [lastPercentage, setLastPercentage] = useState(0);
   const [currentPercentage, setCurrentPercentage] = useState(0);
 
@@ -23,52 +23,55 @@ const SlidingImageTrack = () => {
     if (e.nativeEvent instanceof MouseEvent) {
       setMouseDownAt(e.nativeEvent.clientX)
     }
-    /* if (e.nativeEvent instanceof TouchEvent) {      
-    } */
+    if (e.nativeEvent instanceof TouchEvent) { 
+      const touchPos = e.nativeEvent.touches[0]
+      setMouseDownAt(touchPos.clientX)
+    }
   }
 
-  const handleMouseUp = (e: React.MouseEvent | React.TouchEvent) => {
-    if (e.nativeEvent instanceof MouseEvent) {
-      setMouseDownAt(0) //to prevent movement without clickDown (inMouseMove there is a condition)
+  const handleMouseUp = () => {
+      setMouseDownAt(null) //to prevent movement without clickDown (inMouseMove there is a condition)
       setLastPercentage(currentPercentage)
-    }
-    /* if (e.nativeEvent instanceof TouchEvent) {      
-    } */
   }
 
   const handleMouseMove = (e: React.MouseEvent | React.TouchEvent) => {
+
+    if (mouseDownAt === null) return
+    let mouseDelta;
+
     if (e.nativeEvent instanceof MouseEvent) {
-      if (mouseDownAt === 0) return
-      const mouseDelta = mouseDownAt - e.nativeEvent.clientX //difference in distances of mouseDown and move
-      const deltaMax = window.innerWidth / 2 //because the movement up to half screen will translate 100% of track
-
-      /* const factor = */  //this is the factor to be multiplied and adjust the amount of x translation of the track
-      const percentage = -(mouseDelta / deltaMax) /* * -100 */; //percentage to be translated
-      const unconstrainedPercentage = percentage + lastPercentage
-      const constrainedPercentage = Math.max(Math.min(unconstrainedPercentage, 0), -1) //to do not slide infinitely the track    
-
-      setCurrentPercentage(constrainedPercentage)
-
-      trackRef.current!.animate({
-        transform: `translate(${currentPercentage * trackRef.current!.scrollWidth}px, -50%)`
-      }, {
-        duration: 1200,
-        fill: "forwards"
-      })
-
-      for (const image of trackRef.current!.getElementsByClassName("imageSlider")) {
-        image.animate({
-          objectPosition: `${(1 + constrainedPercentage) * 100}% center`
-        }, { duration: 1200, fill: "forwards" });
-      }
-
+      mouseDelta = mouseDownAt - e.nativeEvent.clientX //difference in distances of mouseDown and move
     }
-    /* if (e.nativeEvent instanceof TouchEvent) {      
-    } */
+
+    if (e.nativeEvent instanceof TouchEvent) {
+      const touchPos = e.nativeEvent.touches[0]
+      mouseDelta = mouseDownAt - touchPos.clientX //difference in distances of touchStart and move
+    }
+
+    const deltaMax = window.innerWidth / 2 //because the movement up to half screen will translate 100% of track
+
+    const percentage = -(mouseDelta! / deltaMax) /* * -100 */; //percentage to be translated
+    const unconstrainedPercentage = percentage + lastPercentage
+    const constrainedPercentage = Math.max(Math.min(unconstrainedPercentage, 0), -1) //to do not slide infinitely the track    
+
+    setCurrentPercentage(constrainedPercentage)
+
+    trackRef.current!.animate({
+      transform: `translate(${currentPercentage * trackRef.current!.scrollWidth}px, -50%)`
+    }, {
+      duration: 1200,
+      fill: "forwards"
+    })
+
+    for (const image of trackRef.current!.getElementsByClassName("imageSlider")) {
+      image.animate({
+        objectPosition: `${(1 + currentPercentage) * 100}% center`
+      }, { duration: 1200, fill: "forwards" });
+    }
   }
 
   return (
-    <section className=' h-screen bg-black overflow-hidden relative' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove}>
+    <section className=' h-screen bg-black overflow-hidden relative' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove} onTouchMove={handleMouseMove} onTouchStart={handleMouseDown} onTouchEnd={handleMouseUp}>
       <div id="image-track" className='flex gap-[4vmin] absolute left-1/2 top-1/2 -translate-y-1/2 select-none' ref={trackRef}>
         {sliderImages.map((image, idx) => {
           return <img src={image} draggable="false" className='w-[40vmin] h-[56vmin] object-cover object-center imageSlider' key={idx}></img>
