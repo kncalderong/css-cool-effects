f9be4e1a231286e599e25272ae735ea99faf2ebb
Kevin Nicolas <kncalderong@gmail.com>
2023-09-13 17:09:49 -0500

    refactor: some types refactor for build step on deployment

--- src/components/CarouselPortfolio/CarouselPortfolio.tsx
+++ src/components/CarouselPortfolio/CarouselPortfolio.tsx
@@ -1,76 +1,114 @@
-import { faTwitter, faYoutube, faCodepen } from "@fortawesome/free-brands-svg-icons"
-import { faDumpsterFire } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import ArticleSlider from "./ArticleSlider"
-import data from "./data"
-import { useEffect, useState } from "react"
-
-
+import {
+  faTwitter,
+  faYoutube,
+  faCodepen,
+} from '@fortawesome/free-brands-svg-icons'
+import { faDumpsterFire } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ArticleSlider from './ArticleSlider'
+import data from './data'
+import { useState } from 'react'
 
 const CarouselPortfolio = () => {
-  
   const [currentIndex, setCurrentIndex] = useState<number>(0)
 
   const calcNext = (number: number) => {
     const next = number + 1 === data.length ? 0 : number + 1
     return next
   }
-  
+
   const calcPrevious = (number: number) => {
     const previous = number - 1 < 0 ? data.length - 1 : number - 1
     return previous
-  } 
-  
+  }
+
   const handleCarousel = (action: string) => {
-    
-    if (action === "next") {
+    if (action === 'next') {
       const nextSlide = calcNext(currentIndex)
       setCurrentIndex(nextSlide)
     }
-    if (action === "prev") {
+    if (action === 'prev') {
       const previousSlide = calcPrevious(currentIndex)
       setCurrentIndex(previousSlide)
     }
   }
-  
+
   return (
-    <section className="bg-[rgb(6,11,25)] w-full h-screen flex flex-col overflow-hidden">
-      <nav className="flex w-full border-b-[1px] border-b-border-portfolio">
-        <div className="navSection flex justify-start items-center px-12 py-8 border-l-[1px] border-l-border-portfolio w-1/3">
-          <a href="#" className="text-white hover:text-hover-highlight-portfolio font-rubik uppercase">
-            <FontAwesomeIcon icon={faDumpsterFire} className="text-5xl" />
+    <section className='bg-[rgb(6,11,25)] w-full h-screen flex flex-col overflow-hidden'>
+      <nav className='flex w-full border-b-[1px] border-b-border-portfolio'>
+        <div className='navSection flex justify-start items-center px-12 py-8 border-l-[1px] border-l-border-portfolio w-1/3'>
+          <a
+            href='#'
+            className='text-white uppercase hover:text-hover-highlight-portfolio font-rubik'
+          >
+            <FontAwesomeIcon icon={faDumpsterFire} className='text-5xl' />
           </a>
         </div>
-        <div className="navSection flex justify-center items-center px-12 py-8 border-l-[1px] border-l-border-portfolio w-1/3 gap-24">
-          <a href="#" className="text-white hover:text-hover-highlight-portfolio font-rubik uppercase">About</a>
-          <a href="#" className="text-white hover:text-hover-highlight-portfolio font-rubik uppercase">Work</a>
+        <div className='navSection flex justify-center items-center px-12 py-8 border-l-[1px] border-l-border-portfolio w-1/3 gap-24'>
+          <a
+            href='#'
+            className='text-white uppercase hover:text-hover-highlight-portfolio font-rubik'
+          >
+            About
+          </a>
+          <a
+            href='#'
+            className='text-white uppercase hover:text-hover-highlight-portfolio font-rubik'
+          >
+            Work
+          </a>
         </div>
-        <div className="navSection flex justify-center items-center px-12 py-8 border-l-[1px] border-l-border-portfolio grow gap-12">
-          <a href="#" className="text-white hover:text-hover-highlight-portfolio font-rubik uppercase">
+        <div className='navSection flex justify-center items-center px-12 py-8 border-l-[1px] border-l-border-portfolio grow gap-12'>
+          <a
+            href='#'
+            className='text-white uppercase hover:text-hover-highlight-portfolio font-rubik'
+          >
             <FontAwesomeIcon icon={faTwitter} />
           </a>
-          <a href="#" className="text-white hover:text-hover-highlight-portfolio font-rubik uppercase">
+          <a
+            href='#'
+            className='text-white uppercase hover:text-hover-highlight-portfolio font-rubik'
+          >
             <FontAwesomeIcon icon={faYoutube} />
           </a>
-          <a href="#" className="text-white hover:text-hover-highlight-portfolio font-rubik uppercase">
-          <FontAwesomeIcon icon={faCodepen} /></a>
+          <a
+            href='#'
+            className='text-white uppercase hover:text-hover-highlight-portfolio font-rubik'
+          >
+            <FontAwesomeIcon icon={faCodepen} />
+          </a>
         </div>
-        <div className="navSection flex justify-center items-center px-12 py-8 border-l-[1px] border-l-border-portfolio grow">
-          <a href="#" className="text-white hover:text-hover-highlight-portfolio font-rubik uppercase">Get in touch</a>
+        <div className='navSection flex justify-center items-center px-12 py-8 border-l-[1px] border-l-border-portfolio grow'>
+          <a
+            href='#'
+            className='text-white uppercase hover:text-hover-highlight-portfolio font-rubik'
+          >
+            Get in touch
+          </a>
         </div>
       </nav>
-      <div className="grow relative overflow-hidden">
+      <div className='relative overflow-hidden grow'>
         {data.map((item, itemIdx) => {
-          let position = "nextSlide"          
+          let position = 'nextSlide'
           if (currentIndex === itemIdx) {
-            position = "activeSlide"
+            position = 'activeSlide'
           }
-          if (itemIdx === currentIndex - 1 ||
+          if (
+            itemIdx === currentIndex - 1 ||
             (currentIndex === 0 && itemIdx === data.length - 1)
           ) {
-            position = "prevSlide"
-          }                    
-          return <ArticleSlider textInfo={item.textInfo} bgImage={item.bgImage} title={item.title} key={item.id} position={position} handleCarousel={handleCarousel} />
+            position = 'prevSlide'
+          }
+          return (
+            <ArticleSlider
+              textInfo={item.textInfo}
+              bgImage={item.bgImage}
+              title={item.title}
+              key={item.id}
+              position={position}
+              handleCarousel={handleCarousel}
+            />
+          )
         })}
       </div>
     </section>
--- src/components/SlidingImageTrack/SlidingImageTrack.tsx
+++ src/components/SlidingImageTrack/SlidingImageTrack.tsx
@@ -1,21 +1,20 @@
 import { useRef, useState } from 'react'
 
 const sliderImages = [
-  "https://images.unsplash.com/photo-1524781289445-ddf8f5695861?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-  "https://images.unsplash.com/photo-1610194352361-4c81a6a8967e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80",
-  "https://images.unsplash.com/photo-1618202133208-2907bebba9e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-  "https://images.unsplash.com/photo-1495805442109-bf1cf975750b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-  "https://images.unsplash.com/photo-1548021682-1720ed403a5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-  "https://images.unsplash.com/photo-1496753480864-3e588e0269b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2134&q=80",
-  "https://images.unsplash.com/photo-1613346945084-35cccc812dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1759&q=80",
-  "https://images.unsplash.com/photo-1516681100942-77d8e7f9dd97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+  'https://images.unsplash.com/photo-1524781289445-ddf8f5695861?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+  'https://images.unsplash.com/photo-1610194352361-4c81a6a8967e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80',
+  'https://images.unsplash.com/photo-1618202133208-2907bebba9e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+  'https://images.unsplash.com/photo-1495805442109-bf1cf975750b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+  'https://images.unsplash.com/photo-1548021682-1720ed403a5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+  'https://images.unsplash.com/photo-1496753480864-3e588e0269b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2134&q=80',
+  'https://images.unsplash.com/photo-1613346945084-35cccc812dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1759&q=80',
+  'https://images.unsplash.com/photo-1516681100942-77d8e7f9dd97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
 ]
 
 const SlidingImageTrack = () => {
-
-  const [mouseDownAt, setMouseDownAt] = useState<number | null>(null); //usf snippet
-  const [lastPercentage, setLastPercentage] = useState(0);
-  const [currentPercentage, setCurrentPercentage] = useState(0);
+  const [mouseDownAt, setMouseDownAt] = useState<number | null>(null) //usf snippet
+  const [lastPercentage, setLastPercentage] = useState(0)
+  const [currentPercentage, setCurrentPercentage] = useState(0)
 
   const trackRef = useRef<HTMLDivElement>(null)
 
@@ -23,21 +22,20 @@ const SlidingImageTrack = () => {
     if (e.nativeEvent instanceof MouseEvent) {
       setMouseDownAt(e.nativeEvent.clientX)
     }
-    if (e.nativeEvent instanceof TouchEvent) { 
+    if (e.nativeEvent instanceof TouchEvent) {
       const touchPos = e.nativeEvent.touches[0]
       setMouseDownAt(touchPos.clientX)
     }
   }
 
   const handleMouseUp = () => {
-      setMouseDownAt(null) //to prevent movement without clickDown (inMouseMove there is a condition)
-      setLastPercentage(currentPercentage)
+    setMouseDownAt(null) //to prevent movement without clickDown (inMouseMove there is a condition)
+    setLastPercentage(currentPercentage)
   }
 
   const handleMouseMove = (e: React.MouseEvent | React.TouchEvent) => {
-
     if (mouseDownAt === null) return
-    let mouseDelta;
+    let mouseDelta: number
 
     if (e.nativeEvent instanceof MouseEvent) {
       mouseDelta = mouseDownAt - e.nativeEvent.clientX //difference in distances of mouseDown and move
@@ -50,31 +48,63 @@ const SlidingImageTrack = () => {
 
     const deltaMax = window.innerWidth / 2 //because the movement up to half screen will translate 100% of track
 
-    const percentage = -(mouseDelta! / deltaMax) /* * -100 */; //percentage to be translated
+    const percentage = -(mouseDelta! / deltaMax) /* * -100 */ //percentage to be translated
     const unconstrainedPercentage = percentage + lastPercentage
-    const constrainedPercentage = Math.max(Math.min(unconstrainedPercentage, 0), -1) //to do not slide infinitely the track    
+    const constrainedPercentage = Math.max(
+      Math.min(unconstrainedPercentage, 0),
+      -1
+    ) //to do not slide infinitely the track
 
     setCurrentPercentage(constrainedPercentage)
 
-    trackRef.current!.animate({
-      transform: `translate(${currentPercentage * trackRef.current!.scrollWidth}px, -50%)`
-    }, {
-      duration: 1200,
-      fill: "forwards"
-    })
+    trackRef.current!.animate(
+      {
+        transform: `translate(${
+          currentPercentage * trackRef.current!.scrollWidth
+        }px, -50%)`,
+      },
+      {
+        duration: 1200,
+        fill: 'forwards',
+      }
+    )
 
-    for (const image of trackRef.current!.getElementsByClassName("imageSlider")) {
-      image.animate({
-        objectPosition: `${(1 + currentPercentage) * 100}% center`
-      }, { duration: 1200, fill: "forwards" });
+    for (const image of trackRef.current!.getElementsByClassName(
+      'imageSlider'
+    )) {
+      image.animate(
+        {
+          objectPosition: `${(1 + currentPercentage) * 100}% center`,
+        },
+        { duration: 1200, fill: 'forwards' }
+      )
     }
   }
 
   return (
-    <section className=' h-screen bg-black overflow-hidden relative' onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove} onTouchMove={handleMouseMove} onTouchStart={handleMouseDown} onTouchEnd={handleMouseUp}>
-      <div id="image-track" className='flex gap-[4vmin] absolute left-1/2 top-1/2 -translate-y-1/2 select-none' ref={trackRef}>
+    <section
+      className='relative h-screen overflow-hidden bg-black '
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+      onTouchMove={handleMouseMove}
+      onTouchStart={handleMouseDown}
+      onTouchEnd={handleMouseUp}
+    >
+      <div
+        id='image-track'
+        className='flex gap-[4vmin] absolute left-1/2 top-1/2 -translate-y-1/2 select-none'
+        ref={trackRef}
+      >
         {sliderImages.map((image, idx) => {
-          return <img src={image} draggable="false" className='w-[40vmin] h-[56vmin] object-cover object-center imageSlider' key={idx}></img>
+          return (
+            <img
+              src={image}
+              draggable='false'
+              className='w-[40vmin] h-[56vmin] object-cover object-center imageSlider'
+              key={idx}
+            ></img>
+          )
         })}
       </div>
     </section>
